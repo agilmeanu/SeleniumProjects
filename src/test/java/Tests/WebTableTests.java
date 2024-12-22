084b4c9b278276695ebf14bc1986b11239f30338
AGilmeanu <agilmeanu-ext@visionbank.com.sa>
2024-12-22 16:54:47 +0200

    acces browser, opne the web page and make the browser in maximaze size

---
+++ src/test/java/Tests/WebTableTests.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class WebTableTests {
+        public WebDriver driver; //variabila globala in Selenium - un tip de data speciala pentru Selenium
+    @Test    //este metoda de test
+        public void WebTableTests() {
+                driver = new ChromeDriver(); //deschide un browser - pe Edge nu da access din ThreadLocker
+                driver.get("https://demoqa.com");  // accesam o pagina web
+                driver.manage().window().maximize(); //facem browserul in modul maximize
+
+    }
+}
